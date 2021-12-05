c52d4a05f26e2ccc87fa87b55f7d97c7ee092f02
Arimario Jesus <arimario.jesus@hotmail.com>
2021-12-05 17:10:01 -0300

    feat: add _document

---
+++ src/pages/_document.tsx
@@ -0,0 +1,26 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          
+          <meta charSet="UTF-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          
+          <title>ari.news</title>
+
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,14 @@
+import styles from '../styles/home.module.scss';
+
+import Head from 'next/head';
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <Head>
+        <title>Home | ari.news</title>
+      </Head>
+      <h1 className={styles.title}>Hello World</h1>
+    </>
   );
 }
