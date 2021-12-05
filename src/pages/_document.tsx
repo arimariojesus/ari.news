263528fce99575b8e5c0524111fa064adb5b7fe3
Arimario Jesus <arimario.jesus@hotmail.com>
2021-12-05 18:02:45 -0300

    feat: add Header component

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,14 +7,13 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
           
           <meta charSet="UTF-8" />
           <meta http-equiv="X-UA-Compatible" content="IE=edge" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          
-          <title>ari.news</title>
 
-          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
+          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
         </Head>
         <body>
           <Main />
