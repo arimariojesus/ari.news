be6e69657be0d04d58ceb9bb15a8dff6c8a89bce
Arimario Jesus <arimario.jesus@hotmail.com>
2021-12-06 19:54:44 -0300

    refactor(Home): changes rendering to static

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,10 +8,6 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
-          
-          <meta charSet="UTF-8" />
-          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
           <link rel="shortcut icon" href="/favicon.png" type="image/png" />
         </Head>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
@@ -39,10 +39,8 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  const price = await stripe.prices.retrieve('price_1K3jrbEqWGXRfaqnehpr86Z8', {
-    expand: ['product'],
-  });
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1K3jrbEqWGXRfaqnehpr86Z8');
 
   const product = {
     priceId: price.id,
@@ -56,5 +54,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24,
   };
 };
