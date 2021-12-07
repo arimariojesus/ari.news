ae7ed0a49c2e29b914116efbef876ee0ae73f878
Arimario Jesus <arimario.jesus@hotmail.com>
2021-12-07 00:19:37 -0300

    feat: integrates auth with fauna db

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from 'faunadb';
+
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -8,4 +12,23 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
   ],
+  jwt: {
+    secret: process.env.SIGNING_KEY,
+  },
+  callbacks: {
+    async signIn({ user }) {
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email: user.email } },
+          ),
+        );
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 });
