680943b8deab809655425b7294103da196a5d652
Arimario Jesus <arimario.jesus@hotmail.com>
2021-12-06 22:40:42 -0300

    feat: add authentication with next auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,21 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const { data: session, status } = useSession();
 
-  return isUserLoggedIn ? (
+  return status === 'authenticated' ? (
     <button
       type="button"
       tabIndex={0}
       className={styles.signInButton}
+      onClick={() => signOut()}
     >
       <FaGithub color="#04d361" aria-hidden="true" />
-      Arimário Jesus
+      {session?.user.name}
       <FiX color="#737380" aria-label="Close Icon" />
     </button>
   ) : (
@@ -21,6 +23,7 @@ export function SignInButton() {
       type="button"
       tabIndex={0}
       className={styles.signInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417" />
       Sign in with Github
